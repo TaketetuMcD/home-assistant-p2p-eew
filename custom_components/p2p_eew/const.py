0f93b1cdabbff95b1eaf132f2890d111ca33864e
TaketetuMcD <taketetu.pc1217@gmail.com>
2026-09-01 02:12:07 +0900

    Add integration constants

---
+++ custom_components/p2p_eew/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "p2p_eew"
+CONF_AREA = "area"
+
+WS_URL = "wss://api.p2pquake.net/v2/ws"
+HISTORY_URL = "https://api.p2pquake.net/v2/history"
+
+EVENT_WARNING = "p2p_eew_warning"
+EVENT_CANCEL = "p2p_eew_cancel"
+
+AUTO_CLEAR_SECONDS = 60
+TEST_CLEAR_SECONDS = 15
+
+# WebSocket reconnect recovery.
+RECOVERY_LIMIT = 20
+RECOVERY_MAX_AGE_SECONDS = 300
